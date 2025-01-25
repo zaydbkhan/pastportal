eaca4f26fae3f780f1bf43fcf368fcadbc80c12b
zaydbkhan <zaydbkhan@gmail.com>
2025-01-25 08:32:30 -0800

    create first iteration of models

--- mapviewer/models.py
+++ mapviewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Waypoint(models.Model):
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+    create_dt = models.DateTimeField()
+    update_dt = models.DateTimeField()
+
+class Image(models.Model):
+    embed_link = models.URLField()
+    description = models.TextField()
+    waypoint = models.ForeignKey(Waypoint, on_delete=models.CASCADE)
+    create_dt = models.DateTimeField()
+    update_dt = models.DateTimeField() # don't know if users will ever "update" an image, but leaving this here for good practice
