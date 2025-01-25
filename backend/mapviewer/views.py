721d7a79f6fd8a2b7addf5d64a672200e8011784
zaydbkhan <zaydbkhan@gmail.com>
2025-01-25 11:46:39 -0800

    update views

--- backend/mapviewer/views.py
+++ backend/mapviewer/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import WaypointSerializer, ImageSerializer
-from .models import Waypoint, Image
+from .serializers import UserSerializer, WaypointSerializer, ImageSerializer
+from .models import User, Waypoint, Image
 
 # Create your views here.
-class WaypointView(viewsets.ModelViewSet):
-    serializer_class = WaypointSerializer
-    queryset = Waypoint.objects.all()
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 class WaypointView(viewsets.ModelViewSet):
     serializer_class = WaypointSerializer
