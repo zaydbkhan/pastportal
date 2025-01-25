52f7b31fe4bbb8be8c960c5d3bc8c58a0a17e36c
zaydbkhan <zaydbkhan@gmail.com>
2025-01-25 12:22:58 -0800

    specify unique attributes

--- backend/mapviewer/models.py
+++ backend/mapviewer/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=32)
+    username = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=32)
     create_dt = models.DateTimeField()
     update_dt = models.DateTimeField()
 
 class Waypoint(models.Model):
-    latitude = models.CharField(max_length=20)
-    longitude = models.CharField(max_length=20)
+    latitude = models.CharField(max_length=20, unique=True)
+    longitude = models.CharField(max_length=20, unique=True)
     private = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     create_dt = models.DateTimeField()
     update_dt = models.DateTimeField()
 
 class Image(models.Model):
-    embed_link = models.URLField()
+    embed_link = models.URLField(unique=True)
     description = models.TextField()
     private = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
