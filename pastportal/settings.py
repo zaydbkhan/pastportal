edd1b3bca18b3608b0c6789586c56ebb6b8d6ee9
zaydbkhan <zaydbkhan@gmail.com>
2025-01-24 23:11:08 -0800

    protect my mysql password

--- pastportal/settings.py
+++ pastportal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,7 +79,7 @@ DATABASES = {
         "ENGINE": "mysql.connector.django",
         "NAME": "pastportal",
         "USER": "root",
-        "PASSWORD": "admin",
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
