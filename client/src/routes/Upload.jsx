a5537c8d9bd9c07ea41545b4fe1bafdecca6aad2
Varshini Vijay <varshinivijay05@gmail.com>
2025-01-25 22:21:36 -0800

    changes

--- client/src/routes/Upload.jsx
+++ client/src/routes/Upload.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-const Upload = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle the form submission here (e.g., file upload logic)
-        console.log("Form submitted");
-    };
+const Upload = () => {
 
     return (
         <div className="container">
             <div className="header">
                 <div className="text">Upload</div>
             </div>
-            <form onSubmit={handleSubmit}>
-                <div className="inputs">
-                    <div className="input">
-                        <input type="file" accept="image/*" placeholder="Image" required />
-                    </div>
-                    <div className="input">
-                        <input type="text" placeholder="Location" required />
-                    </div>
+            <div className="inputs">
+                <div className="input">
+                    <input type="file" accept="image/*" placeholder="Image" required />
                 </div>
-                <button type="submit">Submit</button>
-            </form>
+                <div className="input">
+                    <input type="text" placeholder="Location" required />
+                </div>
+            </div>
         </div>
     );
 };
+
+export default Upload;
