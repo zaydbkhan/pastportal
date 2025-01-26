7963f2f1779d7bdf40fd4764b42a47ce25350b9c
zaydbkhan <zaydbkhan@gmail.com>
2025-01-25 20:35:25 -0800

    postImage working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,48 @@ function App() {
   const [allWayPoints, setAllWayPoints] = useState([]);
   const [imagesArray, setImagesArray] = useState({});
 
+  // post variables
+  const [postSearching, setPostReturnValue] = useState(false);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const postImage = async (embed_link, description, waypoint, create_dt, update_dt) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+        const response = await fetch('http://localhost:8000/api/images/', {
+            method: 'POST',
+            credentials: "include",
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "embed_link": embed_link,
+                "description": description,
+                "waypoint": waypoint,
+                "create_dt": create_dt,
+                "update_dt": update_dt
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const stringifiedData = JSON.stringify(data, null, 2); // Pretty print JSON
+        setPostReturnValue(stringifiedData);
+    } catch (error) {
+        console.error('Error posting waypoints:', error);
+        setError(error.message);
+    } finally {
+        setIsLoading(false);
+    }
+  };
+
+
+
   useEffect(() => {
     const fetchImages = async (waypointId) => {
       try {
@@ -166,7 +208,6 @@ function App() {
     return (
       <Router>
         <link href="https://fonts.googleapis.com/css2?family=Playwrite+AU+SA:wght@100..400&display=swap" rel="stylesheet"></link>
-
         <div className="App">
           <Routes>
             <Route
@@ -231,8 +272,6 @@ function App() {
                                   ))}
                                 </Slider>
                                 <p>Date: 12/14</p>
-
-                                <Link to={`/post/${wp.id}`}>
                                   <button
                                     style={{
                                       backgroundColor: '#581c14',
@@ -248,7 +287,6 @@ function App() {
                                   >
                                     Upload images
                                   </button>
-                                </Link>
                               </div>
                             </Popup>
                           </Marker>
